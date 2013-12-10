bb724e47f4c8efaff851924aa3a2da1407cf8c79
BBenj <balga.benjamin@gmail.com>
2013-12-10 12:19:57 +0100

    Added TOR inputs driver

---
+++ firmware/tordriver.h
@@ -0,0 +1,27 @@
+
+
+#ifndef TOR_DRIVER_H_
+#define TOR_DRIVER_H_
+
+#define TOR_INPUTS_MAX	6
+
+#define TOR_SW1_PORT_NUM	0
+#define TOR_SW1_BIT_NUM		0
+
+#define TOR_SW2_PORT_NUM	0
+#define TOR_SW2_BIT_NUM		1
+
+#define TOR_SW3_PORT_NUM	0
+#define TOR_SW3_BIT_NUM		2
+
+#define TOR_SW4_PORT_NUM	0
+#define TOR_SW4_BIT_NUM		3
+
+#define TOR_SW5_PORT_NUM	0
+#define TOR_SW5_BIT_NUM		4
+
+#define TOR_SW6_PORT_NUM	0
+#define TOR_SW6_BIT_NUM		5
+
+
+#endif // TOR_DRIVER_H_
